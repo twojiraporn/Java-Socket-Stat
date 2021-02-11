a960c183bcd2d8db3ee4145327b6df1ddbb8a05c
Sirawit Yuwasirinun <nobsirawit@gmail.com>
2021-02-11 16:06:21 +0700

    fix error all file

--- src/Client.java
+++ src/Client.java
@@ -9,78 +9,95 @@ public class Client {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        String message="";
-
-        System.out.println("Please select the number : \n" + "1 (Mean)\n" +
-                "2 (Med)\n" +
-                "3 (Mode)\n" +
-                "4 (SD)\n" +
-                "5 (Deviation)\n" +
-                "6 (Factorial)\n" );
-
-        int operation = sc.nextInt();
-
-        if(operation>=1 && operation<=5){
-            String all = "";
-            System.out.println("Please fill value and end with \"end\".");
-
-            String input = sc.next();
-            int checkFirst = 0;
-
-            while (!input.equals("end")) {
-                if (checkFirst == 0) {
-                    all += input;
-                    checkFirst++;
-                } else {
-                    all += " ";
-                    all += input;
-                }
-                input = sc.next();
-            }
-            message = "/"+Integer.toString(operation)+"/"+ all+"/";
-        }
-
-        else if(operation == 6){
-            System.out.println("Please fill only single value.");
-            String input = sc.next();
-            input += " ";
-            message = "/"+Integer.toString(operation)+"/"+ input +"/";
-        }
-
-        else if(operation>=7 || operation==0) {
-            System.out.println("Wrong input operation.");
-            message = "/"+Integer.toString(operation)+"/"+ 0 +"/";
-        }
-
-        //System.out.println(message);
-
         try {
-            socket = new Socket("127.0.0.1", 8999);
+            while(true) {
+                String message = "";
 
-            OutputStream os = socket.getOutputStream();
-            OutputStreamWriter osw = new OutputStreamWriter(os);
-            BufferedWriter bw = new BufferedWriter(osw);
+                System.out.println("Please select the number : \n" + "1 (Mean)\n" +
+                        "2 (Med)\n" +
+                        "3 (Mode)\n" +
+                        "4 (SD)\n" +
+                        "5 (Deviation)\n" +
+                        "6 (Factorial)\n" +
+                        "0 (Exit)\n" );
+
+                int operation = sc.nextInt();
+
+                if (operation >= 1 && operation <= 5) {
+                    String all = "";
+                    System.out.println("Please fill value and end with \"end\".");
+
+                    String input = sc.next();
+                    int checkFirst = 0;
+
+                    while (!input.equals("end")) {
+                        if (checkFirst == 0) {
+                            all += input;
+                            checkFirst++;
+                        } else {
+                            all += " ";
+                            all += input;
+                        }
+                        input = sc.next();
+                    }
+                    message = "/" + Integer.toString(operation) + "/" + all + "/";
+                } else if (operation == 6) {
+                    System.out.println("Please fill only single value.");
+                    String input = sc.next();
+                    input += " ";
+                    message = "/" + Integer.toString(operation) + "/" + input + "/";
+                } else if (operation >= 7) {
+                    //System.out.println("Wrong input operation.");
+                    message = "/" + Integer.toString(operation) + "/" + 0 + "/";
+                }
+                else if(operation == 0){
+                    System.out.println("You already exit");
+                    message = "/" + Integer.toString(operation) + "/" + 0 + "/";
+                    socket = new Socket("127.0.0.1", 8999);
+
+                    OutputStream os = socket.getOutputStream();
+                    OutputStreamWriter osw = new OutputStreamWriter(os);
+                    BufferedWriter bw = new BufferedWriter(osw);
 
 
-            String sendMessage = message + "\n";
-            bw.write(sendMessage);
-            bw.flush();
-            System.out.println();
-            System.out.println("Message sent to the server : " + sendMessage);
+                    String sendMessage = message + "\n";
+                    bw.write(sendMessage);
+                    bw.flush();
+                    break;
+                }
 
-            InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String msg = br.readLine();
-            System.out.println("Answer from server : " + msg);
-        } catch (Exception exception) {
+                //System.out.println(message);
+
+                //try {
+                socket = new Socket("127.0.0.1", 8999);
+
+                OutputStream os = socket.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os);
+                BufferedWriter bw = new BufferedWriter(osw);
+
+
+                String sendMessage = message + "\n";
+                bw.write(sendMessage);
+                bw.flush();
+                System.out.println();
+                System.out.println("Message sent to the server : " + sendMessage);
+
+                InputStream is = socket.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                String msg = br.readLine();
+                System.out.println("Answer from server : " + msg +"\n");
+                // }
+            }
+        }
+        catch (Exception exception) {
             exception.printStackTrace();
         } finally {
             try {
                 socket.close();
             } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println("Wrong Input");
+//                e.printStackTrace();
+ //               System.out.println("You already exit");
             }
         }
     }
--- src/Server.java
+++ src/Server.java
@@ -7,12 +7,16 @@ public class Server {
 
     public static void main(String[] args) {
         double answer = 0;
+        int count = 0;
         try {
             ServerSocket serverSocket = new ServerSocket(8999);
             System.out.println("Server Started and listening to the port 8999\n");
             while (true) {
                 socket = serverSocket.accept();
-                System.out.println("Client connected.");
+                if(count == 0){
+                    System.out.println("Client connected.");
+                    count++;
+                }
                 InputStream is = socket.getInputStream();
                 InputStreamReader isr = new InputStreamReader(is);
                 BufferedReader br = new BufferedReader(isr);
@@ -44,16 +48,24 @@ public class Server {
                     BufferedWriter bw = new BufferedWriter(osw);
                     bw.write(returnMessage + "\n");
                     System.out.println("Answer sent to the client : " + returnMessage + "\n");
-                    System.out.println("Client disconnected : \n");
                     bw.flush();
-                } else if(operation>=7 || operation==0){
-                    String returnMessage = "Wrong input operation.";
+                } else if(operation>=7 || operation<0 ){
+                    String returnMessage = "You input wrong operation.";
                     OutputStream os = socket.getOutputStream();
                     OutputStreamWriter osw = new OutputStreamWriter(os);
                     BufferedWriter bw = new BufferedWriter(osw);
                     bw.write(returnMessage + "\n");
                     System.out.println("Answer sent to the client : " + returnMessage + "\n");
-                    System.out.println("Client disconnected : \n");
+                    bw.flush();
+                }
+                else if(operation == 0){
+                    count = 0;
+                    String returnMessage = "Client exit.";
+                    OutputStream os = socket.getOutputStream();
+                    OutputStreamWriter osw = new OutputStreamWriter(os);
+                    BufferedWriter bw = new BufferedWriter(osw);
+                    bw.write(returnMessage + "\n");
+                    System.out.println("Answer sent to the client : " + returnMessage + "\n");
                     bw.flush();
                 }
             }
@@ -62,7 +74,7 @@ public class Server {
             e.printStackTrace();
         } finally {
             try {
-                String returnMessage = "Server shut down.";
+                String returnMessage = "Client exit.";
                 OutputStream os = socket.getOutputStream();
                 OutputStreamWriter osw = new OutputStreamWriter(os);
                 BufferedWriter bw = new BufferedWriter(osw);
